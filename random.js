329dc8676470907bf9b918dd1d7756d19f249f6d
rirawann85 <56500370+rirawann85@users.noreply.github.com>
2020-02-17 18:57:55 +0700

    Create random.js

---
+++ random.js
@@ -0,0 +1,42 @@
+var pages = [ 
+
+"https://bayartagihandapatkomisi.blogspot.com/"
+"https://bebastopup.blogspot.com/"
+"https://bisnisagenpulsakuota.blogspot.com/"
+"https://bisnisgratispulsagram.blogspot.com/"
+"https://bisnisibupalingsukses.blogspot.com/"
+"https://jualgrosirpulsatermurah.blogspot.com/"
+"https://jualpaketdataonline.blogspot.com/"
+"https://jualpulsaalloperatortermurah.blogspot.com/"
+"https://jualpulsadatamurah.blogspot.com/"
+"https://jualpulsadataonline.blogspot.com/"
+"https://jualpulsaelektrikpalingmurah.blogspot.com/"
+"https://jualpulsahponline.blogspot.com/"
+"https://jualpulsainternetmurah.blogspot.com/"
+"https://jualpulsamurahdancepat.blogspot.com/"
+"https://jualpulsapaketdata.blogspot.com/"
+"https://jualsaldopulsa.blogspot.com/"
+"https://daftaragenpulsakuota.blogspot.com/"
+"https://daftaragenpulsatermurah.blogspot.com/"
+"https://daftarbisatopup.blogspot.com/"
+"https://daftarbisnispulsa.blogspot.com/"
+"https://daftarhargajualpulsa.blogspot.com/"
+"https://daftarhargapulsaalloperator.blogspot.com/"
+"https://daftarhargapulsalistrik.blogspot.com/"
+"https://daftarhargapulsaonline.blogspot.com/"
+"https://daftarhargapulsatelkomsel.blogspot.com/"
+"https://daftarjualpulsaalloperator.blogspot.com/"
+"https://daftarjualpulsaonline.blogspot.com/"
+"https://daftarpulsa3.blogspot.com/"
+"https://daftarpulsakuota.blogspot.com/"
+"https://daftarhargaagenpulsatermurah.blogspot.com/"
+"https://daftarserverpulsaterbaik.blogspot.com/"
+"https://dagangppob.blogspot.com/"
+"https://diskonbelipulsa.blogspot.com/"
+"https://distributoragenpulsatermurah.blogspot.com/"
+
+]; 
+function randomPage() { 
+return pages[Math.round(Math.random() * (pages.length - 1))]; 
+} 
+location.href= randomPage(); 
